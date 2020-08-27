4fecc9e930241d5938178ba8b4f49d7fcca1f386
Dinar Valeev <dinar.valeev@absa.africa>
2020-08-27 16:57:40 +0200

    parameterize namespace names to fall under prj
    Signed-off-by: Dinar Valeev <dinar.valeev@absa.africa>

--- main.tf
+++ main.tf
@@ -2,16 +2,12 @@
 data "rancher2_cluster" "cluster" {
   name = "${var.cluster_name}"
 }
-resource "rancher2_project" "jfrog" {
+resource "rancher2_project" "project" {
     cluster_id = "${data.rancher2_cluster.cluster.id}"
     name = "${var.prj_name}"
 }
-resource "rancher2_namespace" "jfog" {
-    name = "artifactory-ha"
-    project_id = "${rancher2_project.jfrog.id}"
+resource "rancher2_namespace" "namespace" {
+    for_each = toset("${var.namespaces}")
+    name = each.value
+    project_id = "${rancher2_project.project.id}"
 }
-resource "rancher2_namespace" "xray" {
-    name = "xray-ha"
-    project_id = "${rancher2_project.jfrog.id}"
-}
-
--- variables.tf
+++ variables.tf
@@ -2,3 +2,6 @@ variable "cluster_name" {}
 variable "token_key" {}
 variable "rancher_url" {}
 variable "prj_name" {}
+variable "namespaces" {
+  type = list
+}
